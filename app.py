5d8f7e995b57f8a9a1af212644cbe713abb8a713
ADITYA SRINIVAS MENON <adityasrinivas.menon@gmail.com>
2021-07-18 13:33:33 +0530

    initial

---
+++ app.py
@@ -0,0 +1,27 @@
+import os
+
+from flask import Flask, render_template, request, redirect
+
+from main import main 
+from commons import format_class_name
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files.get('file')
+        if not file:
+            return
+        with open(file,'r') as f:
+            text = f.read()
+        main(text = text)
+        return render_template('result.html')
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
---
+++ main.py
@@ -0,0 +1,8 @@
+# vishal code here
+
+
+def main(text):
+  # update database
+
+
+  # do not return anything
